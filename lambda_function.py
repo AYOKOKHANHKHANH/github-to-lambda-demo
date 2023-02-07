9136cef7a2dd4ea048f4664f14684ef47415e349
Trần Khanh <95088817+AYOKOKHANHKHANH@users.noreply.github.com>
2023-02-07 09:06:53 +0700

    added demo lambda function code

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,9 @@
 import json
 def lambda_handler(event, context):
-    return tong2so(event["a"],event["b"])
+    return bieuthuc(event["a"],event["b"])
 
 def tong2so(a,b):
     return a+b
+
+def bieuthuc(a,b):
+    return tong2so(a,b) * tong2so(a,b) + a - b
